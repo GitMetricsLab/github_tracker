369d354358c2a5eab9d12ea8947247a4bea9cfd4
gouravKJ <gouravjaiswal2006@gmail.com>
2025-07-31 00:21:24 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,16 @@ app.use(passport.session());
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
+// Error handling middleware
+app.use((req,res,next) => {
+    res.status(404).json({ message: 'Not Found' });
+});
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {}).then(() => {
     console.log('Connected to MongoDB');

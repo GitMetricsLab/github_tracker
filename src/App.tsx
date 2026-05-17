c02c372311cae5319163f53ee9a9f3299c3770a8
Kartikey <kartikeypathak08@gmail.com>
2026-05-17 19:39:44 +0530

    fix: remove unintended app changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,6 @@ function App() {
   const isFullscreen = FULLSCREEN_ROUTES.includes(location.pathname);
 
   return (
-    <ThemeWrapper>
-      <div className="relative dark:bg-gray-800 flex flex-col min-h-screen">
-        <ScrollProgressBar />
       <div className="relative flex flex-col min-h-screen">
         {!isFullscreen && <ScrollProgressBar />}
 

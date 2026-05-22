f173f1308e7b207b2ff8f56c2007bee425d304d0
KGFCH2 <babinbid05@gmail.com>
2026-05-23 01:48:57 +0530

    docs: add basic TSDoc to useDebounce hook

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 
+/** Debounces a value */
 export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 

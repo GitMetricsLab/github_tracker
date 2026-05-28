f01e345d97603a9eac6c75542889f82da9b46f8c
AnishCoder2006 <anishhhh2006@gmail.com>
2026-05-28 08:08:44 +0530

    feat(ui): add smooth scrolling and active section highlighting for homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,29 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Hero from "../../components/Hero";
 import HowItWorks from "../../components/HowItWorks";
 import Features from "../../components/Features";
 
 function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!location.hash) return;
+
+    const id = location.hash.replace("#", "");
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  }, [location.hash]);
+
   return (
     <div className="">
-        <Hero />
-        <Features />
-        <HowItWorks />
-
+      <Hero />
+      <Features />
+      <HowItWorks />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
